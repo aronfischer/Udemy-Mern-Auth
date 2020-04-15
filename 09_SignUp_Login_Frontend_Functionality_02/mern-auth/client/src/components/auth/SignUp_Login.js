7bac4c745038109fdd4e027dee5a47d6451e803e
Aron Fischer <aronfischersigl@googlemail.com>
2020-04-15 21:58:39 +0200

    create sign up login functionality 02

---
+++ 09_SignUp_Login_Frontend_Functionality_02/mern-auth/client/src/components/auth/SignUp_Login.js
@@ -0,0 +1,185 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import "../../styles/authForm.css";
+
+const SignUpLogin = (props) => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const [styles, setStyles] = useState({
+    login: { left: "50px" },
+    signUp: { left: "400px" },
+    btn: { left: "0" },
+  });
+
+  const { username, email, password } = values;
+  const { start, isDismissible, displayAuth } = props;
+
+  useEffect(() => {
+    if (start === "login") {
+      changeToLogin();
+    } else if (start === "signUp") {
+      changeToSignUp();
+    }
+
+    // Add Event Listeners
+    if (isDismissible) {
+      document.addEventListener("mousedown", handleOutsideClick);
+
+      return () => {
+        document.removeEventListener("mousedown", handleOutsideClick);
+      };
+    }
+  }, []);
+
+  const node = useRef();
+
+  const handleOutsideClick = (e) => {
+    if (node.current.contains(e.target) || !isDismissible) {
+      // Inside click
+      return;
+    } else {
+      // Outside click
+      displayAuth(null);
+    }
+  };
+
+  const changeToSignUp = () => {
+    setStyles({
+      login: { left: "-400px" },
+      signUp: { left: "50px" },
+      btn: { left: "110px" },
+    });
+  };
+
+  const changeToLogin = () => {
+    setStyles({
+      login: { left: "50px" },
+      signUp: { left: "400px" },
+      btn: { left: "0" },
+    });
+  };
+
+  const handleChange = (name) => (event) => {
+    if (name === "username") {
+      setValues({ ...values, username: event.target.value });
+    } else if (name === "email") {
+      setValues({ ...values, email: event.target.value });
+    } else if (name === "password") {
+      setValues({ ...values, password: event.target.value });
+    }
+  };
+
+  return (
+    <div>
+      <SignUpLoginForm
+        values={values}
+        styles={styles}
+        handleChange={handleChange}
+        changeToLogin={changeToLogin}
+        changeToSignUp={changeToSignUp}
+        node={node}
+      />
+    </div>
+  );
+};
+
+const SignUpLoginForm = (props) => {
+  const { username, email, password } = props.values;
+  const { login, signUp, btn } = props.styles;
+  const { handleChange, changeToLogin, changeToSignUp, node } = props;
+
+  return (
+    <>
+      <div className='af-form-wrapper'>
+        <div className='af-form-box' ref={node}>
+          <div className='af-button-box'>
+            <div id='af-btn' style={btn}></div>
+            <button
+              type='button'
+              className='af-toggle-btn'
+              onClick={changeToLogin}
+            >
+              Login
+            </button>
+            <button
+              type='button'
+              className='af-toggle-btn'
+              onClick={changeToSignUp}
+            >
+              Sign up
+            </button>
+          </div>
+
+          <form id='af-login-form' className='af-input-group' style={login}>
+            <div className='af-input-wrapper'>
+              <input
+                type='email'
+                className='af-input-field'
+                value={email}
+                onChange={handleChange("email")}
+              />
+              <label>Email</label>
+            </div>
+            <div className='af-input-wrapper'>
+              <input
+                type='password'
+                className='af-input-field'
+                value={password}
+                onChange={handleChange("password")}
+              />
+              <label>Password</label>
+            </div>
+            <button type='submit' className='af-submit-btn'>
+              Login
+            </button>
+            <button type='button' className='af-forgot-password-btn'>
+              Forgot your Password?
+            </button>
+          </form>
+
+          <form id='af-sign-up-form' className='af-input-group' style={signUp}>
+            <div className='af-input-wrapper'>
+              <input
+                type='text'
+                className='af-input-field'
+                value={username}
+                onChange={handleChange("username")}
+              />
+              <label>Username</label>
+            </div>
+            <div className='af-input-wrapper'>
+              <input
+                type='email'
+                className='af-input-field'
+                value={email}
+                onChange={handleChange("email")}
+              />
+              <label>Email</label>
+            </div>
+            <div className='af-input-wrapper'>
+              <input
+                type='password'
+                className='af-input-field'
+                value={password}
+                onChange={handleChange("password")}
+              />
+              <label>Password</label>
+            </div>
+            <button type='submit' className='af-submit-btn'>
+              Sign up
+            </button>
+            <button type='button' className='af-forgot-password-btn'>
+              Forgot your Password?
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUpLogin;
---
+++ 09_SignUp_Login_Frontend_Functionality_02/mern-auth/client/src/core/Layout.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import Navigation from "./Navigation";
+import SignUpLogin from "../components/auth/SignUp_Login";
+
+import ReactNotification, { store } from "react-notifications-component";
+import "react-notifications-component/dist/theme.css";
+import { defaultSettings } from "../data/notification";
+import "animate.css";
+
+const Layout = () => {
+  const [isShowingAuth, setIsShowingAuth] = useState(null);
+  const [isDismissible, setIsDismissible] = useState(true);
+
+  const displayAuth = (form) => {
+    setIsShowingAuth(form);
+  };
+
+  const createNotification = (notification, next) => {
+    store.createNotification({
+      ...defaultSettings,
+      type: notification.type,
+      title: notification.title,
+      message: notification.message,
+    });
+
+    if (next) {
+      next();
+    }
+  };
+
+  return (
+    <>
+      <Navigation displayAuth={displayAuth} />
+      <ReactNotification />
+      {isShowingAuth === "login" ? (
+        <SignUpLogin
+          start='login'
+          isDismissible={isDismissible}
+          displayAuth={displayAuth}
+        />
+      ) : null}
+      {isShowingAuth === "signUp" ? (
+        <SignUpLogin
+          start='signUp'
+          isDismissible={isDismissible}
+          displayAuth={displayAuth}
+        />
+      ) : null}
+    </>
+  );
+};
+
+export default Layout;
