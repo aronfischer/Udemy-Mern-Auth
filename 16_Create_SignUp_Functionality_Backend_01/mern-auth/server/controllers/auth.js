f69bb14c90a7b62f1abfab52159d70ab85bc6d9e
Aron Fischer <aronfischersigl@googlemail.com>
2020-04-19 16:09:10 +0200

    create signup functionality 01

---
+++ 16_Create_SignUp_Functionality_Backend_01/mern-auth/server/controllers/auth.js
@@ -0,0 +1,52 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+exports.signUp = (req, res) => {
+  const { username, email, password } = req.body;
+
+  User.findOne({ email }, (err, user) => {
+    if (err) {
+      console.log(
+        "ERROR ON FIND USER IN SIGN_UP IN SERVER/CONTROLLERS/AUTH",
+        err
+      );
+
+      return res.status(400).json({
+        errorMsg: "Something went wrong. Please try again.",
+      });
+    }
+
+    if (user) {
+      return res.status(400).json({
+        errorMsg:
+          "Ups, it seems like a user with thi email address already exists.",
+      });
+    }
+
+    if (!user) {
+      // Create a token
+      const signUpToken = jwt.sign(
+        { username, email, password },
+        process.env.TOKEN_SIGNUP_SECRET,
+        { expiresIn: "1h" }
+      );
+
+      // Create email data
+      const sendgridEmail = {
+        from: process.env.EMAIL_FROM,
+        to: email,
+        subject: "Account activation. Please verify your account.",
+        html: `
+          <h1 style="text-align:center" >Please verify your email address</h1>
+          <h3 style="text-align:center">Congratulations, you successfully created an account. There is only one step missing. Please verify your account.</h3>
+          <a href=${process.env.CLIENT_URL}/auth/${signUpToken} style="text-align:center; display=block; margin-bottom=30px">Verify your account</a>
+          <br />
+          <a href=${process.env.CLIENT_URL}/auth/${signUpToken} style="text-align:center; display=block; margin-bottom=30px">${process.env.CLIENT_URL}/auth/${signUpToken}</a>
+          <p style="text-align:center" >This email might contain sensitive Information, do not share it with others</p>
+          `,
+      };
+
+      // Send the email
+    }
+  });
+};
